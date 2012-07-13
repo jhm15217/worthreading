01a11a0f38421455ccf0d337008503b560ea2db4
Michael Villena <mvillena3@gatech.edu>
2012-07-13 17:09:41 -0400

    Converted followers/followed to subscribers/subscribed

--- db/migrate/20120713174541_add_subscribers_to_relationships.rb
+++ db/migrate/20120713174541_add_subscribers_to_relationships.rb
@@ -3,6 +3,9 @@ class AddSubscribersToRelationships < ActiveRecord::Migration
     add_column :relationships, :subscriber_id, :integer
     add_column :relationships, :subscribed_id, :integer
 
+    remove_column :relationships, :follower_id
+    remove_column :relationships, :followed_id
+
     add_index :relationships, :subscriber_id
     add_index :relationships, :subscribed_id
     add_index :relationships, [:subscribed_id, :subscriber_id], unique: true
