da14f6be704bf16392508ad262bc94cdfca93280
Michael Villena <mvillena3@gatech.edu>
2012-07-20 15:56:18 -0400

    Fixing links

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,10 @@ class UserMailer < ActionMailer::Base
     @user = user
 
     # NOTE Try and find a way to determine host name versus manually defining host domain
-    @url  = "https://evening-fog-9503.herokuapp.com/users/#{user.id}/confirm/#{user.confirmation}"
+    @url  = "https://evening-fog-9503.herokuapp.com/users/#{user.id}/confirm/#{user.confirmation_token}"
+    @url2 =  confirm_email_url(host: "evening-fog-9503.herokuapp.com", 
+                               id: user.id, 
+                               confirmation_token: user.confirmation_token)
     mail(to: user.email, subject: "Welcome to Worth Reading")
   end
 end
