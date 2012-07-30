395f919718dbc0885f4a620c26ae460b2a2a01e5
Michael Villena <mvillena3@gatech.edu>
2012-07-30 17:27:58 -0400

    Actual beacon gif

--- app/controllers/wr_logs_controller.rb
+++ app/controllers/wr_logs_controller.rb
@@ -95,13 +95,12 @@ class WrLogsController < ApplicationController
     if @wr_log.action == "email"
       @wr_log.toggle!(:responded)
       @wr_log.action = "opened"
-      puts @wr_log.valid?
       @wr_log.save
       @wr_log.reload
       UserMailer.delay.alert_change_in_wr_log(@wr_log)
       puts "WrLog[#{@wr_log.id}]: Action after save '#{@wr_log.action}'"
     end
 
-    send_file Rails.root.join("public", "images", "test.jpeg"), type: "image/jpeg", disposition: "inline"
+    send_file Rails.root.join("public", "images", "beacon.gif"), type: "image/gif", disposition: "inline"
   end
 end
