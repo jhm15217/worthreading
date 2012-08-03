66da6e5c56d063d10564969f1eeaeabf071ecd29
Michael Villena <mvillena3@gatech.edu>
2012-08-03 15:00:10 -0400

    Password reset views

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,10 +4,14 @@ class PasswordResetsController < ApplicationController
 
   def create
     user =  User.find_by_email(params[:email])
-    user.send_password_reset if user
-
-    redirect_to signin_path, 
-      flash: { notice: "Email sent with password reset instructions." }
+    if user && user.confirmed
+      user.send_password_reset 
+      redirect_to signin_path, 
+        flash: { notice: "Email sent with password reset instructions." }
+    else
+      flash.now { error: "Sorry, that email is not registered" }
+      render new
+    end
   end
 
   def edit
@@ -18,6 +22,12 @@ class PasswordResetsController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user
+    if @user.password_reset_sent < 1.hour.ago
+      redirect_to signin_path, flash: { notice: "Your password reset has expired" }
+    elsif @user.update_attributes(params[:user])
+      redirect_to signin_path, flash: { success: "Your password has been reset" }
+    else
+      render :edit
+    end
   end
 end
