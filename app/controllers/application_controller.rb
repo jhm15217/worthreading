d69a0009f50ec0a948a2713a81db05114be9019e
Michael Villena <mvillena3@gatech.edu>
2012-08-08 17:37:52 -0400

    Working page for retreiving the recipients of an email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   include UsersHelper
+
+  # default_settings for WillPaginate
+  WillPaginate.per_page = 15
 end
