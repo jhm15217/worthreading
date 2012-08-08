ceda88491ea1c8bfd1b18b8b2895ecdc3103dbc8
Michael Villena <mvillena3@gatech.edu>
2012-08-08 16:20:19 -0400

    Alter database queries to work for postgres

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,25 +5,14 @@ class StaticPagesController < ApplicationController
       @emails = @user.emails
       @email_count = @user.emails.count
       @subscribers = @user.subscribers
-      if Rails.env.production?
-        @subscriber_list = @subscribers.map do |subscriber|
-          {name: subscriber.name,
-            email: subscriber.email,
-            id: subscriber.id,
-            sent: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id}").count,
-          opened: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id} and opened IS NOT NULL").count,
-          liked: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id} and worth_reading IS NOT NULL").count } 
-        end.sort_by {|h| -h[:liked] }
-      else
-        @subscriber_list = @subscribers.map do |subscriber|
-          {name: subscriber.name,
-            email: subscriber.email,
-            id: subscriber.id,
-            sent: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id}").count,
-          opened: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id} and opened").count,
-          liked: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id} and worth_reading").count } 
-        end.sort_by {|h| -h[:liked] }
-      end
+      @subscriber_list = @subscribers.map do |subscriber|
+        {name: subscriber.name,
+          email: subscriber.email,
+          id: subscriber.id,
+          sent: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id}").count,
+        opened: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id} and opened IS NOT NULL").count,
+        liked: WrLog.where("sender_id = #{@user.id} and receiver_id = #{subscriber.id} and worth_reading IS NOT NULL").count } 
+      end.sort_by {|h| -h[:liked] }
     end
     @users = User.all order: 'likes DESC'
   end
