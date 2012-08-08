a2c14e4f1572d6d483ead818ce403ae714901094
Michael Villena <mvillena3@gatech.edu>
2012-08-08 15:32:38 -0400

    Changed subscriber input to text area, tweaked regex for capturing email parts

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -47,15 +47,17 @@ class RelationshipsController < ApplicationController
     end
   end
 
-  private
+#  private
 
   def email_address_list(email_addresses)
     x = email_addresses.split(/,\s*/).map{|x| email_address_parts(x) }
     x.select {|x| x}
   end
 
+  # Captures formats i.e. "John Doe"<johndoe@example.com>, John Doe<johndoe@example.com>,
+  # johndoe@example.com
   def email_address_parts(email_address)
-    if parts = (email_address.match('"([^"]*)"<(.*)>') or email_address.match('([-a-z0-9.]*)<(.*)>'))
+    if parts = (email_address.match('"([^"]*)"<(.*)>') or email_address.match('([a-zA-Z\s.]*)<(.*)>'))
       name = parts.captures[0]
       email_address = parts.captures[1]
     else
