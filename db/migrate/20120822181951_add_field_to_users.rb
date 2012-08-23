05a6edacc82a7db84cd29c9da657a758052f122f
James Morris <james.morris@cmu.edu>
2012-08-22 23:20:30 -0400

    fix migrate bug

--- db/migrate/20120822181951_add_field_to_users.rb
+++ db/migrate/20120822181951_add_field_to_users.rb
@@ -1,6 +1,6 @@
 class AddFieldToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :email_notify, :boolean { default: true }
-    add_column :users, :cohort, :integer {default: 0 }
+    add_column :users, :email_notify, :boolean, { default: true }
+    add_column :users, :cohort, :integer, {default: 0 }
   end
 end
