6a89a00d8d5f6d35d2c0e1cdf8fcf62bb6cf1c46
Michael Villena <mvillena3@gatech.edu>
2012-09-10 14:01:30 -0400

    Changed url for chrome extension

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': 'http://www.worth-reading.org'}, function(tab) {
+  chrome.tabs.create({'url': 'http://www.worth-reading.org/chrome_extension/new'}, function(tab) {
 		console.log("Tab opened")
     // Tab opened.
   });
