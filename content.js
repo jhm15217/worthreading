86c8183a55cdde1e174836ad8b4380705f75cf55
Michael Villena <mvillena3@gatech.edu>
2012-09-12 13:11:36 -0400

    Grab url

--- content.js
+++ content.js
@@ -1,6 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': 'http://www.worth-reading.org/chrome_extension/new'}, function(tab) {
-		console.log("Tab opened")
-    // Tab opened.
+  chrome.tabs.create({'url': 'http://www.worth-reading.org/chrome_extension/new?link=' + tab.url }, function(tab) {
   });
 });
