e8fb273bcfeb5686a5b75ed62cdfbbd7aba6aa4d
Michael Villena <mvillena3@gatech.edu>
2012-09-13 15:04:31 -0400

    Attempt to get chrome extension to get selected text

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': 'http://www.worth-reading.org/chrome_extension/new?link=' + tab.url }, function(tab) {
+  alert(window.getSelection().toString())
+  chrome.tabs.create({'url': 'http://localhost:3000/chrome_extension/new?link=' + window.getSelection().toString() + '&subject=' + tab.title }, function(tab) {
   });
-});
+//   chrome.tabs.create({'url': 'http://www.worth-reading.org/chrome_extension/new?link=' + tab.url + '&subject=' + tab.title }, function(tab) {
+//   });
+})
