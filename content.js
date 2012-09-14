87ffb7a4fe0d62ec576fb20dcebac0a2d0e47ec7
Michael Villena <mvillena3@gatech.edu>
2012-09-14 13:06:01 -0400

    Finding the subject will be done by rails app

--- content.js
+++ content.js
@@ -5,17 +5,16 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.selection) {
-    var subject = request.selection.match(/^.{120,}?[.?!]+(?=\s|$)/) 
 
     chrome.tabs.create({
       'url': 'http://www.worth-reading.org/chrome_extension/new?text=' + request.selection 
-      + '&link=' + request.url + '&subject=' + subject
+      + '&link=' + request.url
     });
 
     // Development mode 
     // chrome.tabs.create({
     //   'url': 'http://localhost:3000/chrome_extension/new?text=' + request.selection 
-    //   + '&link=' + request.url + '&subject=' + subject
+    //   + '&link=' + request.url 
     // });
   } else  {
     console.log("Unknown request")
