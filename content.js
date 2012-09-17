7084240bdfe9b397ab840e28da9bc634daf007b8
Michael Villena <mvillena3@gatech.edu>
2012-09-17 17:14:23 -0400

    Conversion to POST Request but UTF-8 Problem

--- content.js
+++ content.js
@@ -3,17 +3,34 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     code: "chrome.extension.sendRequest({selection: window.getSelection().toString(), url: document.URL});"});
 }); 
 
+function fakePost(text, link) {   
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "http://www.worth-reading.org/chrome_extension/new");
+    var params = {text: text, link: link};
+    for(var key in params) {
+        var hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type", "hidden");
+        hiddenField.setAttribute("name", key);
+        hiddenField.setAttribute("value", params[key]);
+        form.appendChild(hiddenField);
+    }
+    document.body.appendChild(form);
+    form.submit();
+};
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.selection) {
+    fakePostCode = fakePost.toString().replace(/(\n|\t)/gm, '');
     chrome.tabs.query({'url': 'http://www.worth-reading.org/chrome_extension/*'}, function(tab) {
       if (tab[0]) {
-        chrome.tabs.update(tab[0].id, { 'url': 'http://www.worth-reading.org/chrome_extension/new?text=' 
-          + request.selection + '&link=' + request.url, 'active': true
+        chrome.tabs.update(tab[0].id, 
+          { 'url': "javascript:" + fakePostCode + "; fakePost('" + request.selection + "', '" + request.url + "');",
+            'active': true
         });
       } else {
         chrome.tabs.create({
-          'url': 'http://www.worth-reading.org/chrome_extension/new?text=' + request.selection 
-          + '&link=' + request.url
+          'url': "javascript:" + fakePostCode + "; fakePost('" + request.selection + "', '" + request.url + "');"
         });
 
         // Development mode 
@@ -22,7 +39,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         //   + '&link=' + request.url 
         // });
       }
-    });
+      });
   } else  {
     console.log("Unknown request")
   }
