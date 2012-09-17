dc517c13d7a3a7ec9af131ffe43829e4dd8995cc
Michael Villena <mvillena3@gatech.edu>
2012-09-17 14:23:12 -0400

    Extension now reuses worth-reading chrome extension tab instead of creating new ones

--- content.js
+++ content.js
@@ -5,11 +5,11 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.selection) {
-    chrome.tabs.query({'url': 'http://www.worth-reading.org/chrome_extension/new?text='}, function(tab) {
-      if (tab) {
-        chrome.tabs.update(tab.id, { 'url': 'http://www.worth-reading.org/chrome_extension/new' 
-          + request.selection + '&link=' + request.url
-        })
+    chrome.tabs.query({'url': 'http://www.worth-reading.org/chrome_extension/*'}, function(tab) {
+      if (tab[0]) {
+        chrome.tabs.update(tab[0].id, { 'url': 'http://www.worth-reading.org/chrome_extension/new?text=' 
+          + request.selection + '&link=' + request.url, 'active': true
+        });
       } else {
         chrome.tabs.create({
           'url': 'http://www.worth-reading.org/chrome_extension/new?text=' + request.selection 
