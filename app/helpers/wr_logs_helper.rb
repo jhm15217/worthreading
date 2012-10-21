0d1c7a1616f7ce6018815b09e5632ac627314011
James Morris <james.morris@cmu.edu>
2012-10-20 21:18:43 -0400

    typo

--- app/helpers/wr_logs_helper.rb
+++ app/helpers/wr_logs_helper.rb
@@ -1,7 +1,7 @@
 module WrLogsHelper
 
   def best_recent_emails
-    t = WrLog.select('email_id, sender_id, count(forwarded) as forwarded').group('email_id, sender_id').uniq
+    t = WrLog.select('email_id, count(forwarded) as forwarded').group('email_id').uniq
     puts t.inspect
     t = t.sort!{|t1,t2| Email.find(t2.email_id).created_at <=> Email.find(t1.email_id).created_at }.take(500)
     puts t.inspect
