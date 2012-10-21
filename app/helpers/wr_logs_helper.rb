bc62058c6907bbd45e5292a03afcbf14cb0f59b0
James Morris <james.morris@cmu.edu>
2012-10-21 17:00:37 -0400

    google analytics

--- app/helpers/wr_logs_helper.rb
+++ app/helpers/wr_logs_helper.rb
@@ -1,13 +1,9 @@
 module WrLogsHelper
 
   def best_recent_emails
-    t = WrLog.select('email_id, count(forwarded) as forwarded').group('email_id').uniq
-    puts t.inspect
-    t = t.sort!{|t1,t2| Email.find(t2.email_id).created_at <=> Email.find(t1.email_id).created_at }.take(500)
-    puts t.inspect
-    t = t.sort!{|t1,t2|t2.forwarded <=> t1.forwarded}.take(10)
-    puts t.inspect
-    t
+    WrLog.select('email_id, count(forwarded) as forwarded').group('email_id').uniq
+    .sort!{|t1,t2| Email.find(t2.email_id).created_at <=> Email.find(t1.email_id).created_at }.take(500)
+    .sort!{|t1,t2|t2.forwarded <=> t1.forwarded}.take(10)
   end
 
 end
