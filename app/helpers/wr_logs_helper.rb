b9acb4d97547cc85903d3a0c632585ae12774163
James Morris <james.morris@cmu.edu>
2012-10-23 14:25:49 -0400

    try best emails fix

--- app/helpers/wr_logs_helper.rb
+++ app/helpers/wr_logs_helper.rb
@@ -1,9 +1,9 @@
 module WrLogsHelper
 
   def best_recent_emails
-    WrLog.select('email_id, count(forwarded) as forwarded').group('email_id').uniq
+    WrLog.select('email_id, count(forwarded) as liked').group('email_id').uniq
     .sort!{|t1,t2| Email.find(t2.email_id).created_at <=> Email.find(t1.email_id).created_at }.take(500)
-    .sort!{|t1,t2|t2.forwarded <=> t1.forwarded}.take(10)
+    .sort!{|t1,t2|t2.liked <=> t1.liked}.take(10)
   end
 
 end
